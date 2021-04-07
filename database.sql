08302caead2156ee9278017534d80559358db251
Levi <arcanjolevi@gmail.com>
2021-04-07 17:01:47 -0400

    db update and data sampling

--- database.sql
+++ database.sql
@@ -1,143 +1,233 @@
 -- MySQL Workbench Forward Engineering
-SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS,
-  UNIQUE_CHECKS = 0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS,
-  FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE,
-  SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema Pedidos
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `Pedidos` ;
+
 -- -----------------------------------------------------
 -- Schema Pedidos
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Pedidos` DEFAULT CHARACTER SET utf8;
-USE `Pedidos`;
+CREATE SCHEMA IF NOT EXISTS `Pedidos` DEFAULT CHARACTER SET utf8 ;
+USE `Pedidos` ;
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`Bairro`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Bairro` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`Bairro` (
-  `idBairro` INT NOT NULL,
-  `nomeBairro` VARCHAR(45) NULL,
-  PRIMARY KEY (`idBairro`)
-) ENGINE = InnoDB;
+  `idBairro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeBairro` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idBairro`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
--- Table `Pedidos`.`TipoLogradouro`
+-- Table `Pedidos`.`UnidadeFederacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Pedidos`.`TipoLogradouro` (
-  `siglaLogradouro` VARCHAR(10) NOT NULL,
-  `nomeLogradouro` VARCHAR(45) NULL,
-  PRIMARY KEY (`siglaLogradouro`)
-) ENGINE = InnoDB;
--- -----------------------------------------------------
--- Table `Pedidos`.`Logradouro`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Pedidos`.`Logradouro` (
-  `idLogradouro` INT NOT NULL,
-  `nomeLogradouro` VARCHAR(45) NULL,
-  `TipoLogradouro_siglaLogradouro` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`idLogradouro`),
-  INDEX `fk_Logradouro_TipoLogradouro1_idx` (`TipoLogradouro_siglaLogradouro` ASC) VISIBLE,
-  CONSTRAINT `fk_Logradouro_TipoLogradouro1` FOREIGN KEY (`TipoLogradouro_siglaLogradouro`) REFERENCES `Pedidos`.`TipoLogradouro` (`siglaLogradouro`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
--- -----------------------------------------------------
--- Table `Pedidos`.`UnidadeFederaco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Pedidos`.`UnidadeFederaco` (
+DROP TABLE IF EXISTS `Pedidos`.`UnidadeFederacao` ;
+
+CREATE TABLE IF NOT EXISTS `Pedidos`.`UnidadeFederacao` (
   `siglaUf` CHAR(2) NOT NULL,
-  `nomeUf` VARCHAR(45) NULL,
-  PRIMARY KEY (`siglaUf`)
-) ENGINE = InnoDB;
+  `nomeUf` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`siglaUf`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`Cidade`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Cidade` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`Cidade` (
-  `idCidade` INT NOT NULL,
-  `nomeCidade` VARCHAR(45) NULL,
-  `UnidadeFederaco_siglaUf` CHAR(2) NOT NULL,
+  `idCidade` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeCidade` VARCHAR(45) NULL DEFAULT NULL,
+  `UnidadeFederacao_siglaUf` CHAR(2) NOT NULL,
   PRIMARY KEY (`idCidade`),
-  INDEX `fk_Cidade_UnidadeFederaco1_idx` (`UnidadeFederaco_siglaUf` ASC) VISIBLE,
-  CONSTRAINT `fk_Cidade_UnidadeFederaco1` FOREIGN KEY (`UnidadeFederaco_siglaUf`) REFERENCES `Pedidos`.`UnidadeFederaco` (`siglaUf`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+  INDEX `fk_Cidade_UnidadeFederacao1_idx` (`UnidadeFederacao_siglaUf` ASC) VISIBLE,
+  CONSTRAINT `fk_Cidade_UnidadeFederacao1`
+    FOREIGN KEY (`UnidadeFederacao_siglaUf`)
+    REFERENCES `Pedidos`.`UnidadeFederacao` (`siglaUf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `Pedidos`.`TipoLogradouro`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`TipoLogradouro` ;
+
+CREATE TABLE IF NOT EXISTS `Pedidos`.`TipoLogradouro` (
+  `siglaLogradouro` VARCHAR(10) NOT NULL,
+  `nomeLogradouro` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`siglaLogradouro`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `Pedidos`.`Logradouro`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Logradouro` ;
+
+CREATE TABLE IF NOT EXISTS `Pedidos`.`Logradouro` (
+  `idLogradouro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeLogradouro` VARCHAR(45) NULL DEFAULT NULL,
+  `TipoLogradouro_siglaLogradouro` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`idLogradouro`),
+  INDEX `fk_Logradouro_TipoLogradouro1_idx` (`TipoLogradouro_siglaLogradouro` ASC) VISIBLE,
+  CONSTRAINT `fk_Logradouro_TipoLogradouro1`
+    FOREIGN KEY (`TipoLogradouro_siglaLogradouro`)
+    REFERENCES `Pedidos`.`TipoLogradouro` (`siglaLogradouro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`Endereco`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Endereco` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`Endereco` (
-  `idEndereco` INT NOT NULL,
-  `cep` VARCHAR(8) NULL,
-  `Bairro_idBairro` INT NOT NULL,
-  `Logradouro_idLogradouro` INT NOT NULL,
-  `Cidade_idCidade` INT NOT NULL,
+  `idEndereco` INT(11) NOT NULL AUTO_INCREMENT,
+  `cep` VARCHAR(8) NULL DEFAULT NULL,
+  `Bairro_idBairro` INT(11) NOT NULL,
+  `Logradouro_idLogradouro` INT(11) NOT NULL,
+  `Cidade_idCidade` INT(11) NOT NULL,
   PRIMARY KEY (`idEndereco`),
   INDEX `fk_Endereco_Bairro1_idx` (`Bairro_idBairro` ASC) VISIBLE,
   INDEX `fk_Endereco_Logradouro1_idx` (`Logradouro_idLogradouro` ASC) VISIBLE,
   INDEX `fk_Endereco_Cidade1_idx` (`Cidade_idCidade` ASC) VISIBLE,
-  CONSTRAINT `fk_Endereco_Bairro1` FOREIGN KEY (`Bairro_idBairro`) REFERENCES `Pedidos`.`Bairro` (`idBairro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Endereco_Logradouro1` FOREIGN KEY (`Logradouro_idLogradouro`) REFERENCES `Pedidos`.`Logradouro` (`idLogradouro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Endereco_Cidade1` FOREIGN KEY (`Cidade_idCidade`) REFERENCES `Pedidos`.`Cidade` (`idCidade`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+  CONSTRAINT `fk_Endereco_Bairro1`
+    FOREIGN KEY (`Bairro_idBairro`)
+    REFERENCES `Pedidos`.`Bairro` (`idBairro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Endereco_Logradouro1`
+    FOREIGN KEY (`Logradouro_idLogradouro`)
+    REFERENCES `Pedidos`.`Logradouro` (`idLogradouro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Endereco_Cidade1`
+    FOREIGN KEY (`Cidade_idCidade`)
+    REFERENCES `Pedidos`.`Cidade` (`idCidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`Cliente`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Cliente` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`Cliente` (
-  `codCliente` INT NOT NULL,
-  `nomeCliente` VARCHAR(45) NULL,
-  `nroCasa_endCliente` INT NULL,
-  `compl_endCliente` VARCHAR(45) NULL,
-  `Endereco_idEndereco` INT NOT NULL,
+  `codCliente` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeCliente` VARCHAR(45) NULL DEFAULT NULL,
+  `nroCasa_endCliente` INT(11) NULL DEFAULT NULL,
+  `compl_endCliente` VARCHAR(45) NULL DEFAULT NULL,
+  `Endereco_idEndereco` INT(11) NOT NULL,
   PRIMARY KEY (`codCliente`, `Endereco_idEndereco`),
   INDEX `fk_Cliente_Endereco1_idx` (`Endereco_idEndereco` ASC) VISIBLE,
-  CONSTRAINT `fk_Cliente_Endereco1` FOREIGN KEY (`Endereco_idEndereco`) REFERENCES `Pedidos`.`Endereco` (`idEndereco`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+  CONSTRAINT `fk_Cliente_Endereco1`
+    FOREIGN KEY (`Endereco_idEndereco`)
+    REFERENCES `Pedidos`.`Endereco` (`idEndereco`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`PedidoVenda`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`PedidoVenda` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`PedidoVenda` (
-  `nroPedido` INT NOT NULL AUTO_INCREMENT,
-  `dtPedido` DATE NULL,
-  `totoalPedido` DOUBLE NULL,
-  `nroCasa_endEntrega` INT NULL,
-  `compl_endEntrega` VARCHAR(45) NULL,
-  `PedidoVendacol` VARCHAR(45) NULL,
-  `Cliente_codCliente` INT NOT NULL,
-  `Endereco_idEndereco` INT NOT NULL,
+  `nroPedido` INT(11) NOT NULL AUTO_INCREMENT,
+  `dtPedido` DATE NULL DEFAULT NULL,
+  `totoalPedido` DOUBLE NULL DEFAULT NULL,
+  `nroCasa_endEntrega` INT(11) NULL DEFAULT NULL,
+  `compl_endEntrega` VARCHAR(45) NULL DEFAULT NULL,
+  `PedidoVendacol` VARCHAR(45) NULL DEFAULT NULL,
+  `Cliente_codCliente` INT(11) NOT NULL,
+  `Endereco_idEndereco` INT(11) NOT NULL,
   PRIMARY KEY (`nroPedido`, `Endereco_idEndereco`),
   INDEX `fk_PedidoVenda_Cliente1_idx` (`Cliente_codCliente` ASC) VISIBLE,
   INDEX `fk_PedidoVenda_Endereco1_idx` (`Endereco_idEndereco` ASC) VISIBLE,
-  CONSTRAINT `fk_PedidoVenda_Cliente1` FOREIGN KEY (`Cliente_codCliente`) REFERENCES `Pedidos`.`Cliente` (`codCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_PedidoVenda_Endereco1` FOREIGN KEY (`Endereco_idEndereco`) REFERENCES `Pedidos`.`Endereco` (`idEndereco`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+  CONSTRAINT `fk_PedidoVenda_Cliente1`
+    FOREIGN KEY (`Cliente_codCliente`)
+    REFERENCES `Pedidos`.`Cliente` (`codCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PedidoVenda_Endereco1`
+    FOREIGN KEY (`Endereco_idEndereco`)
+    REFERENCES `Pedidos`.`Endereco` (`idEndereco`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`Produto`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`Produto` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`Produto` (
-  `codProduto` INT NOT NULL,
-  `nomeProduto` VARCHAR(45) NULL,
-  `precoProduto` DOUBLE NULL,
-  PRIMARY KEY (`codProduto`)
-) ENGINE = InnoDB;
+  `codProduto` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeProduto` VARCHAR(45) NULL DEFAULT NULL,
+  `precoProduto` DOUBLE NULL DEFAULT NULL,
+  `qtdeEstoque` INT NULL,
+  PRIMARY KEY (`codProduto`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
 -- -----------------------------------------------------
 -- Table `Pedidos`.`ItemPedidoVenda`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `Pedidos`.`ItemPedidoVenda` ;
+
 CREATE TABLE IF NOT EXISTS `Pedidos`.`ItemPedidoVenda` (
-  `qtdeVenda` INT NULL,
-  `precoVenda` DOUBLE NULL,
-  `totalItemProduto` DOUBLE NULL,
-  `PedidoVenda_nroPedido` INT NOT NULL,
-  `Produto_codProduto` INT NOT NULL,
+  `qtdeVenda` INT(11) NULL DEFAULT NULL,
+  `precoVenda` DOUBLE NULL DEFAULT NULL,
+  `totalItemProduto` DOUBLE NULL DEFAULT NULL,
+  `PedidoVenda_nroPedido` INT(11) NOT NULL,
+  `Produto_codProduto` INT(11) NOT NULL,
   PRIMARY KEY (`PedidoVenda_nroPedido`, `Produto_codProduto`),
   INDEX `fk_ItemPedidoVenda_Produto1_idx` (`Produto_codProduto` ASC) VISIBLE,
-  CONSTRAINT `fk_ItemPedidoVenda_PedidoVenda` FOREIGN KEY (`PedidoVenda_nroPedido`) REFERENCES `Pedidos`.`PedidoVenda` (`nroPedido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ItemPedidoVenda_Produto1` FOREIGN KEY (`Produto_codProduto`) REFERENCES `Pedidos`.`Produto` (`codProduto`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB;
--- -----------------------------------------------------
--- Table `Pedidos`.`TipoLogradouro`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Pedidos`.`TipoLogradouro` (
-  `siglaLogradouro` VARCHAR(10) NOT NULL,
-  `nomeLogradouro` VARCHAR(45) NULL,
-  PRIMARY KEY (`siglaLogradouro`)
-) ENGINE = InnoDB;
-SET SQL_MODE = @OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
+  CONSTRAINT `fk_ItemPedidoVenda_PedidoVenda`
+    FOREIGN KEY (`PedidoVenda_nroPedido`)
+    REFERENCES `Pedidos`.`PedidoVenda` (`nroPedido`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ItemPedidoVenda_Produto1`
+    FOREIGN KEY (`Produto_codProduto`)
+    REFERENCES `Pedidos`.`Produto` (`codProduto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- samples.sql
+++ samples.sql
@@ -1,1 +1,116 @@
 -- Datatabase sampling
+
+INSERT INTO Bairro (nomeBairro) VALUES ("Coroado");
+INSERT INTO Bairro (nomeBairro) VALUES ("Indianópolis");
+INSERT INTO Bairro (nomeBairro) VALUES ("Igapó");
+INSERT INTO Bairro (nomeBairro) VALUES ("Setor 03");
+INSERT INTO Bairro (nomeBairro) VALUES ("Rio Doce");
+INSERT INTO Bairro (nomeBairro) VALUES ("Condominio Residencial Camping Club");
+INSERT INTO Bairro (nomeBairro) VALUES ("Jardim Jorge Teixeira");
+INSERT INTO Bairro (nomeBairro) VALUES ("Sao Francisco");
+INSERT INTO Bairro (nomeBairro) VALUES ("Clima Bom");
+INSERT INTO Bairro (nomeBairro) VALUES ("Andra Gadelha");
+INSERT INTO Bairro (nomeBairro) VALUES ("Marinho");
+INSERT INTO Bairro (nomeBairro) VALUES ("Vila Nasser");
+INSERT INTO Bairro (nomeBairro) VALUES ("Residencial Paraiso");
+
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("ES", "Espiritu Santo");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("SP", "Sao Paulo");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("RN", "Rio Grande do Norte");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("RO", "Roraima");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("PE", "Pernambuco");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("MT", "Mato Grosso do Sul");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("AC", "Acre");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("PB", "Paraiba");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("AL", "Alagoas");
+INSERT INTO UnidadeFederacao (siglaUf, nomeUf) VALUES ("MA", "Maranhao");
+
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Guarapari","ES");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("São Paulo", "SP");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Natal", "RN");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Ariquemes", "RO");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Olinda", "PE");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Sinop", "MT");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Ariquemes", "PB");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Rio Branco", "AC");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Maceió", "AL");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Sousa", "MA");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("Campo Grande", "MT");
+INSERT INTO Cidade (nomeCidade, UnidadeFederacao_siglaUf) VALUES ("São Luís", "MA");
+
+
+INSERT INTO TipoLogradouro (siglaLogradouro, nomeLogradouro) VALUES ("R.", "Rua");
+INSERT INTO TipoLogradouro (siglaLogradouro, nomeLogradouro) VALUES ("Av.", "Avenida");
+INSERT INTO TipoLogradouro (siglaLogradouro, nomeLogradouro) VALUES ("Al.", "Alameda");
+
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Atílio Vivacqua", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Indianópolis", "Av.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Irmã Vitória", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("São Paulo", "Al.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Setenta", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("da Matrinchã", "Av.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Açaí", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("A2", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Monsenhor Vicente de Freitas", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Marluce", "R.");
+INSERT INTO Logradouro (nomeLogradouro, TipoLogradouro_siglaLogradouro) VALUES ("Babaçu", "R.");
+
+
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("29203500", 1, 5, 12 );
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("04062002", 2, 4, 11);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("59106029", 3, 3, 10);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("76870340", 4, 2, 9);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("53090500", 5, 1, 8);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("78551233", 6, 6, 7);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("76876520", 7, 7, 6);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("69901525", 8, 8, 5);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("57071560", 9, 9, 4);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("58806380", 10, 10,3 );
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("79117542", 11, 11, 2);
+INSERT INTO Endereco (CEP, Cidade_idCidade, Logradouro_idLogradouro, Bairro_idBairro) VALUES ("65081788", 12, 3, 1);
+
+
+
+
+
+
+
+"Pedro Kevin Isaac Silveira",	
+"582",
+	
+
+"Davi Marcelo da Costa",
+"321"
+  
+"Nicolas Ryan Edson da Rosa",
+"140",
+	
+	
+"Milena Aparecida Nicole Oliveira",
+"730",
+	
+"Larissa Adriana Marcela da Costa",
+"458",
+
+"Catarina Luana Almeida",
+"269",
+	
+	
+"Vitor Benício Paulo Galvão",
+"903",
+	
+"Augusto Ricardo Antonio da Rocha",
+"921",
+	
+"Andreia Eliane Julia Rocha",
+"696",
+	
+"Raul Otávio Benedito Duarte",
+"882",
+	
+"Alessandra Catarina Cláudia Teixeira",
+488,
+
+"Severino Raul Leonardo Farias",
+966,
+	
