e01ded1f67b761110927bfb7ed98def4053c5131
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-04-08 20:30:27 -0300

    Fim

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,22 +1,9 @@
 package project;
 
-import java.sql.SQLException;
-
 public class Main {
 	public static void main(String[] args) {
 
 		DbConnection connection = new DbConnection();
-		// PedidoVenda pedidoVenda = new PedidoVenda(
-		// 		"2021-07-07", 6516.00, 4, 4, 774,
-		// 		"Ao lado da padaria.", "Alguma coisa q eu não sei."
-		// );
-		// Integer[] saleQnt = {3};
-		// Double[] saleCost = {33.00};
-		// Double[] totalProductItem = {11.00};
-		// Integer[] productId = { 5 };
-		// ItemPedidoVenda itemPedidoVenda = new ItemPedidoVenda(
-		// 		saleQnt, saleCost, totalProductItem, productId
-		// );
 		PedidoVenda successCasePedidoVenda = new PedidoVenda(
 				"2021-07-07", 6516.00, 4, 4, 774,
 				"Ao lado da padaria.", "Alguma coisa q eu não sei."
